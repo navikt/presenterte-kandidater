625448397df8fe4375b675cf86e1ccd44a9b332b
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2022-10-13 10:16:04 +0200

    Installer og test designbibliotek

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,26 +1,25 @@
-const Index = () => {
+import { Heading, BodyLong } from "@navikt/ds-react";
+import type { LinksFunction } from "@remix-run/server-runtime";
+import css from "~/styles/index.css";
+
+export const links: LinksFunction = () => {
+    return [{ rel: "stylesheet", href: css }];
+};
+
+const Kandidatlister = () => {
     return (
-        <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-            <h1>Welcome to Remix</h1>
-            <ul>
-                <li>
-                    <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
-                        15m Quickstart Blog Tutorial
-                    </a>
-                </li>
-                <li>
-                    <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
-                        Deep Dive Jokes App Tutorial
-                    </a>
-                </li>
-                <li>
-                    <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-                        Remix Docs
-                    </a>
-                </li>
-            </ul>
-        </div>
+        <main className="kandidatlister">
+            <Heading size="medium">Kandidatlister</Heading>
+            <BodyLong>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+            </BodyLong>
+        </main>
     );
 };
 
-export default Index;
+export default Kandidatlister;
