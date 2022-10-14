8dc3bd86f0884b52b1c3ebca563f0017d9ea0a1a
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2022-10-14 10:42:52 +0200

    Bruk andre lenker i brødsmulesti i dev

--- app/services/dekoratør.tsx
+++ app/services/dekoratør.tsx
@@ -1,24 +1,44 @@
 import { renderToString } from "react-dom/server";
 import { fetchDecoratorReact } from "@navikt/nav-dekoratoren-moduler/ssr";
+import { hentMiljø, Miljø } from "./miljø";
 
-const brødsmulesti = [
-    {
-        title: "Arbeidsgiver",
-        url: "https://www.nav.no/no/bedrift",
-    },
-    {
-        title: "Min side - arbeidsgiver",
-        url: "https://arbeidsgiver.nav.no/min-side-arbeidsgiver/",
-    },
-];
+const hentBrødsmulesti = (miljø: Miljø) => {
+    if (miljø === Miljø.ProdGcp) {
+        return [
+            {
+                title: "Min side - arbeidsgiver",
+                url: "https://arbeidsgiver.nav.no/min-side-arbeidsgiver/",
+            },
+            {
+                title: "Kandidatlister",
+                url: "https://arbeidsgiver.nav.no/kandidatliste/",
+            },
+        ];
+    } else if (miljø === Miljø.DevGcp) {
+        return [
+            {
+                title: "Min side - arbeidsgiver",
+                url: "https://min-side-arbeidsgiver.dev.nav.no/min-side-arbeidsgiver/",
+            },
+            {
+                title: "Kandidatlister",
+                url: "https://presenterte-kandidater.dev.nav.no/kandidatliste/",
+            },
+        ];
+    } else {
+        return [];
+    }
+};
 
 const hentDekoratør = async () => {
+    const miljø = hentMiljø();
+
     const decorator = await fetchDecoratorReact({
         env: process.env.NODE_ENV === "production" ? "prod" : "dev",
         simple: false,
         chatbot: false,
         context: "arbeidsgiver",
-        breadcrumbs: brødsmulesti,
+        breadcrumbs: hentBrødsmulesti(miljø),
     });
 
     const HeaderHtml = renderToString(<decorator.Header />);
