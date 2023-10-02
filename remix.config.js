75b423e26bb763a18a942c3405bca4812c665d1a
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-10-02 13:04:09 +0200

    Oppgrader til Remix 2

--- remix.config.js
+++ remix.config.js
@@ -3,12 +3,4 @@ module.exports = {
     ignoredRouteFiles: ["**/.*", "**/*.module.css"],
     publicPath: "/kandidatliste/build/",
     serverModuleFormat: "cjs",
-    future: {
-        v2_dev: true,
-        v2_errorBoundary: true,
-        v2_routeConvention: true,
-        v2_normalizeFormMethod: true,
-        v2_meta: true,
-        v2_headers: true,
-    },
 };
