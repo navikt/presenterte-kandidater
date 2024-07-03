ba32738f2c2c9335aaceb9485f6676ad888a443b
Erlend Wiklem <admin@wiklem.no>
2024-07-03 11:09:47 +0200

    Håndtere ny dekoratør

--- app/services/dekoratør/useInjectScript.ts
+++ app/services/dekoratør/useInjectScript.ts
@@ -1,11 +1,12 @@
-import { useRef, useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 /*
  * Injiser script-elementet til dekoratøren og en tilhørende div.
  * useEffect()-hooken sørger for at dette gjøres utelukkende client-side,
  * for ellers vil dekoratøren manipulere DOM-en og forstyrre hydreringen.
  */
-const useInjectDecoratorScript = (script?: string) => {
+
+export const useInjectDecoratorScript = (script?: string) => {
     const isInjected = useRef(false);
 
     useEffect(() => {
@@ -13,30 +14,44 @@ const useInjectDecoratorScript = (script?: string) => {
             const parser = new DOMParser();
             const parsedDocument = parser.parseFromString(script, "text/html");
 
-            const parsedElements = Array.from(parsedDocument.body.childNodes);
-            const parsedDivElement = parsedElements[0] as HTMLDivElement;
-            const parsedScriptElement = parsedElements[2] as HTMLScriptElement;
+            const headNodes = Array.from(parsedDocument.head.childNodes);
 
-            const divElement = createElementWithAttributes("div", parsedDivElement.attributes);
-            const scriptElement = createElementWithAttributes(
-                "script",
-                parsedScriptElement.attributes
-            );
+            if (headNodes.length !== 0) {
+                // New
+                headNodes.forEach((node) => {
+                    const htmlScriptElement = node as HTMLScriptElement;
+                    const htmlElement = createHtmlElement("script", htmlScriptElement);
+                    document.body.appendChild(htmlElement);
+                });
+            } else {
+                // Old
+                const parsedElements = Array.from(parsedDocument.body.childNodes);
+                const parsedDivElement = parsedElements[0] as HTMLDivElement;
+                const parsedScriptElement = parsedElements[2] as HTMLScriptElement;
 
-            document.body.appendChild(divElement);
-            document.body.appendChild(scriptElement);
+                const divElement = createHtmlElement("div", parsedDivElement);
+                const scriptElement = createHtmlElement("script", parsedScriptElement);
+
+                document.body.appendChild(divElement);
+                document.body.appendChild(scriptElement);
+            }
 
             isInjected.current = true;
         }
     }, [script]);
 };
 
-const createElementWithAttributes = (tag: string, attributes: NamedNodeMap) => {
+const createHtmlElement = (tag: string, htmlScriptElement: HTMLElement) => {
     const element = document.createElement(tag);
 
-    for (let i = 0; i < attributes.length; i++) {
+    const attributes = htmlScriptElement.attributes;
+    const innerText = htmlScriptElement.innerText;
+
+    for (let i = 0; i < htmlScriptElement.attributes.length; i++) {
         element.setAttribute(attributes[i].name, attributes[i].value);
     }
+
+    element.innerText = innerText;
 
     return element;
 };
