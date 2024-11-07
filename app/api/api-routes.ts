ff8e97b25e45d4472df960a3189752b9f3248f51
Erlend Wiklem <admin@wiklem.no>
2024-11-07 09:06:06 +0100

    Add path

--- app/api/api-routes.ts
+++ app/api/api-routes.ts
@@ -10,13 +10,13 @@ export interface Iroute {
 export const NotifikasjonAPI: Iroute = {
   api_route: '/notifikasjon-api',
   api_url: process.env.NOTIFIKASJON_API ?? '',
-  internUrl: '/api/notifikasjon-bruker-api',
+  internUrl: '/kandidatliste/api/notifikasjon-bruker-api',
   scope: `${getClusterGCP()}:fager:notifikasjon-bruker-api`,
 };
 
 export const PresenterteKandidaterAPI: Iroute = {
   api_route: '/presenterte-kandidater-api',
   api_url: process.env.PRESENTERT_KANDIDATER_API ?? '',
-  internUrl: '/api/presenterte-kandidater-api',
+  internUrl: '/kandidatliste/api/presenterte-kandidater-api',
   scope: `${getClusterGCP()}:toi:presenterte-kandidater-api`,
 };
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   basePath: '/kandidatliste',
+  assetPrefix: '/kandidatliste',
   reactStrictMode: true,
   experimental: {
     optimizePackageImports: ['@navikt/ds-react', '@navikt/aksel-icons'],
