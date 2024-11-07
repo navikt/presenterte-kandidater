f0c6e48293e98732db0a6991864a7af482e1d025
Erlend Wiklem <admin@wiklem.no>
2024-11-07 14:35:14 +0100

    api routes med /kandidatliste

--- app/api/api-routes.ts
+++ app/api/api-routes.ts
@@ -8,14 +8,14 @@ export interface Iroute {
 }
 
 export const NotifikasjonAPI: Iroute = {
-  api_route: '/api',
+  api_route: '/kandidatliste/api',
   api_url: process.env.NOTIFIKASJON_API_URL ?? '',
   internUrl: `/api/notifikasjon-bruker-api`,
   scope: `${getClusterGCP()}:fager:notifikasjon-bruker-api`,
 };
 
 export const PresenterteKandidaterAPI: Iroute = {
-  api_route: '',
+  api_route: '/kandidatliste',
   api_url: process.env.PRESENTERT_KANDIDATER_API ?? '',
   internUrl: `/api/presenterte-kandidater-api`,
   scope: `${getClusterGCP()}:toi:presenterte-kandidater-api`,
