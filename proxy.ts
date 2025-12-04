102690b139d8ef1f342242aa4560ebd008995cb6
Erlend Wiklem <admin@wiklem.no>
2025-12-04 09:31:36 +0100

    Bump next16

--- proxy.ts
+++ proxy.ts
@@ -3,11 +3,11 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 /**
- * This middleware is purely a hack to be able to provide the react server component auth
+ * This proxy is purely a hack to be able to provide the react server component auth
  * with the current path. This is needed to be able to redirect to the correct page after
  * login.
  */
-export async function middleware(request: NextRequest): Promise<NextResponse> {
+export async function proxy(request: NextRequest): Promise<NextResponse> {
   const requestUrl: URL = new URL(request.url);
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set('x-path', requestUrl.pathname + requestUrl.search);
